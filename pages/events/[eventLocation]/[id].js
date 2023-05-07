edfdec0ea9d9e7b1ea1e2071e5b25202afaffbf3
SandeshGC <gcsandesh01@gmail.com>
2023-05-07 19:08:40 +0545

    add data to event page

--- pages/events/[eventLocation]/[id].js
+++ pages/events/[eventLocation]/[id].js
@@ -1,4 +1,41 @@
+import Image from "next/image"
 import React from "react"
-export default function EventPage() {
-	return <div>EventPage</div>
+export default function EventPage({ event }) {
+	return (
+		<div>
+			<Image src={event.image} height={300} width={400} alt={event.title} />
+			<h1>{event.title}</h1>
+			<p>{event.description}</p>
+		</div>
+	)
+}
+
+export async function getStaticPaths(context) {
+	const { allEvents } = await import("/data/data.json")
+
+	const allPaths = allEvents.map((evt) => ({
+		params: {
+			eventLocation: evt.city,
+			id: evt.id,
+		},
+	}))
+
+	return {
+		paths: allPaths,
+		fallback: false,
+	}
+}
+
+export async function getStaticProps(context) {
+	const { allEvents } = await import("/data/data.json")
+	const event = allEvents.find(
+		(evt) =>
+			evt.city === context?.params.eventLocation &&
+			evt.id === context?.params.id
+	)
+	return {
+		props: {
+			event,
+		},
+	}
 }
