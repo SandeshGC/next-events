e776445cfa3d4adc8dd9fb53551ad03ee058c83e
SandeshGC <gcsandesh01@gmail.com>
2023-05-09 21:33:06 +0545

    create about-us page

---
+++ pages/about-us.js
@@ -0,0 +1,21 @@
+import Head from "next/head"
+
+const AboutUsPage = () => {
+	return (
+		<>
+			<Head>
+				<title>About Us</title>
+			</Head>
+			<div>
+				<h1>About Next Events</h1>
+				<p>
+					Next Events is a very useful website where you can discover exciting
+					events happening near you. Get details about various events or create
+					and post an event yourself!
+				</p>
+			</div>
+		</>
+	)
+}
+
+export default AboutUsPage
